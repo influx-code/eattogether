5e1cfc09bed98d9d1bb52043aee769446ed3baae
zhao wen qiang <13143410521@163.com>
2018-10-24 13:48:44 +0800

    add fr_end

--- front_end/src/views/About.vue
+++ front_end/src/views/About.vue
@@ -1,21 +1,13 @@
-<template id="main-page">
-  <v-ons-page>
-    <v-ons-toolbar>
-      <div class="center">Title</div>
-    </v-ons-toolbar>
-
-    <p style="text-align: center">
-      <v-ons-button @click="$ons.notification.alert('Hello World!')">
-        Click me!
-      </v-ons-button>
-    </p>
-  </v-ons-page>
+<template>
+  <h1>About</h1>
 </template>
 
 <style>
-  /* CSS goes here */
+/* CSS goes here */
 </style>
 
 <script>
-  // Javascript goes here
+export default {
+  
+}
 </script>
